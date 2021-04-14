e54fd7e07e0f8c8bde396abc72d4377dfc241bef
wooseok1223 <tjdntjr123@gmail.com>
2021-04-15 00:09:29 +0900

    엑셀 업로드 기본 환경 구성

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,7 @@ from . import views
 router = routers.SimpleRouter(trailing_slash=True)
 router.register(r"signup", views.SingUpViewSet, basename="v1")
 
-
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('upload/', views.CsvUploadView.as_view(), name="upload"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,17 @@
+import io
+
+from django.utils import timezone
+from django.http import HttpResponse
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet, ReadOnlyModelViewSet, mixins
 from . import models
 from . import serializers
 import logging
+from openpyxl import Workbook
+import pandas as pd
+import csv
 
 logger = logging.getLogger(__name__)
 
@@ -32,3 +41,59 @@ class SingUpViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewS
 
 
         return Response({"success": 1,"result": {}})
+
+
+class CsvUploadView(CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        data = request.FILES["file"]
+        df = pd.read_csv(data, encoding='cp949')
+        print(df)
+
+        try:
+            return Response({
+                'success': 1,
+                'message': '엑셀 업로드 성공'
+            })
+        except:
+            return Response({
+                'success': 0
+            })
+
+class CsvDownloadView(ListAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    def list(self, request, *args, **kwargs):
+        response = HttpResponse(
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        response["Content-Disposition"] = "attachment; filename={date}-delivery_information.xlsx".format(
+            date=timezone.now().strftime("%Y-%m-%d"),
+        )
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.title = "Delivery"
+
+        # 컬럼명
+        columns = [
+
+        ]
+        row_num = 1
+
+        #  컬럼 헤더에 타이틀 설정
+        for col_num, column_title in enumerate(columns, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+
+        workbook.save(response)
+
+        try:
+            return Response({
+                'success': 1,
+                'message': '엑셀 업로드 성공'
+            })
+        except:
+            return Response({
+                'success': 0
+            })
